447c877e76d5aab354245aac8a918f16fd5aae90
JungHyeonKang <rkdwjdgus0103@gmail.com>
2024-12-05 12:54:28 +0900

    chore: 프로젝트 기본 세팅
    1. docker 세팅
    2. mysql 세팅
    3. yml 파일 생성

---
+++ studyCafe/src/test/java/com/kang/studyCafe/StudyCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kang.studyCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
